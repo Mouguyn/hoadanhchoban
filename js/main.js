121802adc99a45c28f87a28897ac18cc198e4e5a
Mouguyn <90453855+Mouguyn@users.noreply.github.com>
2024-10-15 23:22:01 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Chúc bạn một ngày lễ vui vẻ nhé').split('')
+    const titles = ('Chúc Kiều Anh một ngày lễ vui vẻ nhé <3 luv u').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
